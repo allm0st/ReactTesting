1801e3093cfa1f7026df4545c43dff832573f788
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-08-16 16:56:55 +0100

    Implements challenges and finishes section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends PureComponent {
+  state = {
+    counter: 0,
+    error: false,
+  };
+
+  // for challenge #2, the behavior for decrement is complicated enough
+  // that I separated the function from the jsx.
+  decrementCounter = () => {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  };
+
+  // for challenge #3, I separated the incrementCounter onClick
+  incrementCounter = () => {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    // determine whether error is hidden based on state
+    const errorClass = this.state.error ? '' : 'hidden';
+
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        <div data-test="error-message" className={`error ${errorClass}`}>
+          The counter cannot go below 0
+        </div>
+        <button data-test="increment-button" onClick={this.incrementCounter}>
+          Increment counter
+        </button>
+        <button data-test="decrement-button" onClick={this.decrementCounter}>
+          Decrement counter
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
